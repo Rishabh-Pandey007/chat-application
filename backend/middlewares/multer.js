028321a4fe711e38f49de215c0741959a36fe1fe
Rishabh-Pandey007 <rishabhpandey2023@gmail.com>
2025-09-10 00:06:31 +0530

    feat: implement uploadFilesToCloudinary for handling file uploads

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,13 +1,12 @@
 import multer from "multer";
 
-
 const multerUpload = multer({
-    limits:{
-        fileSize: 1024*1024*5,
-    },
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
 });
 
-const singleAvatar= multerUpload.single("avatar");
+const singleAvatar = multerUpload.single("avatar");
 
 const attachmentsMulter = multerUpload.array("files", 5);
 
--- backend/utils/features.js
+++ backend/utils/features.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose"
 import jwt from "jsonwebtoken"
+import { v4 as uuid} from "uuid";
+import { v2 as cloudinary } from "cloudinary"
+import { getBase64 } from "../lib/helper.js"
 
 const cookieOptions = {
     maxAge: 15 * 24 * 60 * 60 * 1000,
@@ -27,6 +30,7 @@ const sendToken = (res, user, code, message)=>{
         .cookie("chattu-token", token, cookieOptions)
         .json({
             success:true,
+            user,
             message,
         });
 };
@@ -35,8 +39,39 @@ const emitEvent = (req, event, users, data) => {
     console.log("Emitting event: ", event);
 };
 
+const uploadFilesToCloudinary = async(files = [])=>{
+    const uploadPromises = files.map((file) =>{
+        return new Promise((resolve, reject) =>{
+            cloudinary.uploader.upload(
+                getBase64(file),
+                {
+                    resource_type: "auto",
+                    public_id: uuid(),
+                },
+                (error,result)=>{
+                    if(error) return reject(error);
+                    resolve(result);
+                }
+            );
+        });
+    });
+
+    try{
+        const result = await Promise.all(uploadPromises);
+
+        const formattedResult = result.map((result) =>({
+            public_id: result.public_id,
+            url: result.secure_url,
+        }));
+
+        return formattedResult;
+    } catch(err){
+        throw new Error("Error uploading files to cloudinary", err);
+    }
+};
+
 
 const deleteFilesFromCloudinary = async(public_ids ) =>{
 
 }; 
-export {connectDB, sendToken, cookieOptions, emitEvent, deleteFilesFromCloudinary};
+export {connectDB, sendToken, cookieOptions, emitEvent, deleteFilesFromCloudinary, uploadFilesToCloudinary};
